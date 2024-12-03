065245c3a0cda1db5bf2402d3dddebc301b36ba7
AZIZ <ziz8fhad@gmail.com>
2024-12-03 18:14:43 +0300

    BACK END DONE

--- backend/app.js
+++ backend/app.js
@@ -1,8 +1,22 @@
 import express from "express"
-import passengerController from './controllers/passengerController.js'
+
+import passengerController from './controllers/passengerController.js';
+import reservationController from './controllers/reservationController.js';
+import scheduleController from './controllers/scheduleController.js';
+import stationController from './controllers/stationController.js';
+import ticketController from './controllers/ticketController.js'
+import trainController from './controller/trainController.js'
+
 const app = express();
 
-app.use("/passenger",passengerController)
+app.use("/api/passenger",passengerController)
+app.use("/api/reservation",reservationController )
+app.use("/api/schedule",scheduleController )
+app.use("/api/station",stationController )
+app.use("/api/ticket",ticketController )
+app.use("/api/train",trainController )
+
+
 
 const port = 3333 || process.env.PORTU
 app.listen(port, (req,res) => {
